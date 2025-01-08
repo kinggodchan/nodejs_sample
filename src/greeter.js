593a5b52a62066aad02747f6e4cf41c35982c3b6
kimseongchan <kyc010264@gmail.com>
2025-01-08 15:34:15 +0900

    Greeter 타입 스크립트 소개개

---
+++ src/greeter.js
@@ -0,0 +1,6 @@
+function greeter(person) {
+    return "Hello" + person;
+}
+var user = "seong chan";
+console.log(greeter(user));
+//document.body.textContent = greeter(user);
---
+++ src/greeter.ts
@@ -0,0 +1,9 @@
+function greeter(person: string) {
+    return "Hello" + person;
+}
+
+let user = "seong chan";
+
+console.log(greeter(user));
+
+//document.body.textContent = greeter(user);

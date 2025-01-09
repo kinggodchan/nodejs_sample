1b03e028c806a60d8bb70e1a2cf95325fa411e0d
kimseongchan <kyc010264@gmail.com>
2025-01-09 15:35:40 +0900

    클래스의 정의

---
+++ src/class/class.ts
@@ -0,0 +1,40 @@
+//class 이용
+class Robot { //Members
+    //Property 또는 Field(속성, 필드)
+    name:string;
+    model: string;
+    status: string = "Active";
+
+    //Constructor(생성자)
+    constructor(name: string, model: string) {
+        this.name = name;
+        this.model = model;
+    }
+
+    //Method(행동)
+    performTask(task: string) {
+        console.log(`${this.name} is performing ${task}.`);
+    };
+
+
+    updateStatus(newStatus: string) {
+        this.status = newStatus;
+        console.log(`${this.name}'s status is now ${this.status}.`);
+    };
+}
+
+
+// Create Instance of the Robot class;
+//생성자를 통해서 계속해서 생겨서 가독성이 좋아진다.
+let r1 = new Robot("R2-k1","glalxy")
+let r2 = new Robot("R2-k2","sam sung")
+let r3 = new Robot("R2-k3","lg")
+
+//Accessing fields and Calling methods
+console.log(r1.name);
+console.log(r2.model);
+console.log(r3.status);
+
+r1.performTask("Charging");
+r1.performTask("Explorering");
+r1.updateStatus("On Repair");

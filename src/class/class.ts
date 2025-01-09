cfa5fbfe34cd2a5ab045ec26e1ef0a7f0b871174
kimseongchan <kyc010264@gmail.com>
2025-01-09 15:52:35 +0900

    클래스 사용 양식 방법

--- src/class/class.ts
+++ src/class/class.ts
@@ -3,13 +3,13 @@ class Robot { //Members
     //Property 또는 Field(속성, 필드)
     name:string;
     model: string;
-    status: string = "Active";
+    status: string = "Active"; // 기본값 Default 값을 설정할 수 있다.
 
     //Constructor(생성자)
     //생성자는 필수적인 요소이다.
     //this는 robot이다
     constructor(name: string, model: string) {
-        this.name = name;
+        this.name = name; 
         this.model = model;
     }
 
@@ -54,3 +54,35 @@ class Pet{
 
 let p1 = new Pet();
 console.log(p1.name);
+
+
+class User {
+    //[필드 부분]
+    username: string; //타입만을 표기하는 경우
+    email:string;
+    job:string = "Student"; //기본값을 주는 경우
+
+    //[생성자 부분]
+    constructor(username: string, email: string) {
+        this. username = username; //매개변수로 초기화
+        this. email = email;
+    }
+
+    //[매서드 부분]
+    study(): void {
+        console.log(`${this.username} is studying.`);
+    }
+}
+
+//[객체 생성] (new 키워드와 arguments 입력)
+let user1 = new User("홍길도", "hong@aabc.co.kr");
+let user2 = new User("서길도", "king@dong.com");
+
+//[객체의 사용, 접근] (. 도트연산자 사용)
+//1. 인스턴스 자체 적브
+console.log(user1) // 출력 :User {username:'홍길동', email:'....'}
+//2. 인스턴스의 세부 필드 접근
+console.log(user2.username); //출력: 김철수
+//3.메서드 호출
+user1.study();
+user2.study();
